c2aa821f00d81561148fa3c5232ec770aa8a7879
Andrés Tello <andrest911@gmail.com>
2023-11-20 23:35:24 -0300

    mas Cambios

--- mongoose-demo/public/js/scripts.js
+++ mongoose-demo/public/js/scripts.js
@@ -1,7 +1,39 @@
-(function(){
+(function () {
+    let email
     const socket = io()
 
-    socket.on('Update-messages',(messages)=>{
-        console.log('messages',messages)
+    document.getElementById('form-message')
+        .addEventListener('submit', (event) => {
+            event.preventDefault()
+            const input = document.getElementById('input-message')
+            const newMsg ={
+                email,
+                body:input.value
+            }
+            input.value=''
+            input.focus()
+            socket.emit('new-message',newMsg)
+        })
+
+    socket.on('Update-messages', (messages) => {
+        console.log('messages', messages)
     })
+
+    Swal.fire({
+        title: 'Indentificate por favor 👮',
+        input: 'text',
+        allowOutsideClick: false,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Necesitamos que ingreses su email para continuar.'
+            }
+        }
+    })
+        .then((result) => {
+            email = result.value.trim();
+            console.log(`hola ${email} Bienvenido`);
+        })
+        .catch((error) => {
+            console.error('Ah ocurrido un error al capturar el nombre 😨:', error.message);
+        });
 })()
